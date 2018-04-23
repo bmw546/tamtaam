6b2989d8591aa05169765c03bd052f88db515648
DESKTOP-OHJLOHD\TheTh <thethousandbeanster@hotmail.com>
2018-04-23 15:08:28 -0400

    page suiviDeCommandes

---
+++ SuiviDeCommandes.php
@@ -0,0 +1,30 @@
+<!--****************************************
+Fichier : SuiviDeCommandes.php
+Auteur : Rémi Létourneau
+Fonctionnalité : Gestion des demandes et suivi de livraison
+Date : 2018-04-23
+
+Vérification :
+Date                    Nom 		    Approuvé
+=========================================================
+
+Historique de modifications :
+Date                    Nom             Description
+=========================================================
+
+***********************************************-->
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset='utf-8'/>
+		<title>cible suivi des commandes</title>
+	</head>
+	<body>
+		<?php
+
+
+		echo "<p>".$_POST['orderNumber']."</p>".
+
+		?>
+	</body>
+</html>
