7cbc76ed74870160284a293dbc7ccfae3514e7ec
idremi7 <32065191+idremi7@users.noreply.github.com>
2018-04-23 15:53:20 -0400

    gestionnaireUtilisateur
    création du controlleur et gestionnaire Utilisateur

--- gestionnaireUtilisateur.php
+++ gestionnaireUtilisateur.php
@@ -1,4 +1,5 @@
-<!--****************************************
+<?php
+/********************************************************
 Fichier : gestionnaireUtilisateur.php
 Auteur :  Rémi Létourneau
 Fonctionnalité : Gestion des comptes utilisateurs
@@ -12,14 +13,106 @@ Historique de modifications :
 Date                    Nom             Description
 =========================================================
 
-***********************************************-->
-<?php
-	require_once 'utilisateur.php';
+*********************************************************/
+require_once 'utilisateur.php';
 
-	$aUser = new Utilisateur();
+class GestionnaireUtilisateur {
 
-	//ajoute l'utilisateur a la base de donnée
-	//TODO
+	private $nom_utilisateur;
+	private $mot_de_passe;
+	private $email;
+	private $adresse;
+	private $telephone;
+	
+	/**
+	* Constructeur
+	*/
+	public function __construct($nom_utilisateur, $mot_de_passe, $email, $adresse, $telephone){		
+        $this->nom_utilisateur = $nom_utilisateur;
+        $this->mot_de_passe = $mot_de_passe;
+        $this->email = $email;
+		$this->adresse = $adresse;
+		$this->telephone = $telephone;
+    }
+	
+	/**
+	* modifier le nom d'utilisateur
+	*/
+	public function setNomUtilisateur($nom){
+		$this->nom_utilisateur = $nom;
+	}
+	
+	/**
+	* retourne le nom d'utilisateur
+	*/
+	public function getNomUtilisateur(){
+		return $this->nom_utilisateur;
+	}
 
+	/**
+	* modifie le mot de passe
+	*/
+	public function setMotDePasse($mdp){
+		$this->mot_de_passe = $mdp;
+	}
+	
+	/**
+	* Retourne le mot de passe
+	*/
+	public function getMotDePasse(){
+		return $this->mot_de_passe;
+	}
+	
+	/**
+	* Modifie l'email
+	*/
+	public function setEmail($adresseEmail){
+		$this->email = $adresseEmail;
+	}
+	
+	/**
+	* retourne l'email
+	*/
+	public function getEmail(){
+		return $this->email;
+	}
 
+	/**
+	* modifie l'adresse
+	*/
+	public function setAdresse($adresseMaison){
+		$this->adresse =  $adresseMaison;
+	}
+	
+	/**
+	* retourne l'adresse
+	*/
+	public function getAdresse(){
+		return $this->adresse;
+	}
+
+	/**
+	* modifie le numéro de telephone
+	*/
+	public function setTelephone($tel){
+		$this->telephone =  $tel;
+	}
+	
+	/**
+	* retourne le numéro de téléphone
+	*/
+	public function getTelephone(){
+		return $this->telephone;
+	}
+	
+	/**
+	* Ajouter un utilisateur avec les informations de l'interface inscription
+	*/
+	public function ajouterUtilisateur(){		
+	
+        $aUser = new Utilisateur(0, $this->nom_utilisateur, $this->mot_de_passe, $this->email, $this->adresse, $this->telephone);
+		
+		//ajoute un utilisateur à la base de donnée
+    }
+}
 ?>
