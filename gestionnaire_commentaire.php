f79d0e8f7d9c7906c0829fcf6dd0b9bf0038cfdd
bmw546 <marc-etienne-pepin@hotmail.com>
2018-04-24 15:45:20 -0400

    v2 message

--- gestionnaire_commentaire.php
+++ gestionnaire_commentaire.php
@@ -2,13 +2,15 @@
 class mail{
     function sentMail($nom,$courriel,$sujet,$question){
         $header="MIME-Version: 1.0\r\n";
-		$header.='From:"'.$nom.'""'.$courriel.'"'."\n";
+		$header.='From:"tamtaam.com"<tamTaam@tamtaam.com>'."\n";
 		$header.='Content-Type:text/html; charset="uft-8"'."\n";
 		$header.='Content-Transfer-Encoding: 8bit';
+		//$sujet .= " provient de : " . $nom;
+		//$question .= " provient de l'adresse email suivant : " . $courriel;
 		mail("marc-etienne-pepin@hotmail.com",$sujet,$question,$header);
-		
+		echo "bravo 33";
     }
-
+	
 
 }
 ?>
