5cff47f11363aa3f4dfb8c6cef973e899bea396e
bmw546 <marc-etienne-pepin@hotmail.com>
2018-04-24 15:30:20 -0400

    here it is

--- commentaire.php
+++ commentaire.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+﻿<!DOCTYPE html>
 <html>
 	<head>
 		<meta charset='utf-8'/>
@@ -6,17 +6,24 @@
 	</head>
 	<body>
 		<?php
-		
-		$aUser = new Utilisateur($_POST['user'], $_POST['adresse'], $_POST['email'], 
-		$_POST['noTelephone'], $_POST['passwd']);
+		include ("gestionnaire_commentaire.php");
+		$Email = new mail();
+		//$aUser = new Utilisateur($_POST['user'], $_POST['adresse'], $_POST['email'],
+		//$_POST['noTelephone'], $_POST['passwd']);
 
 		//ajoute l'utilisateur a la base de donn�e
 		//TODO
-
+        /*
 		echo "<p>".$_POST['nom']."</p>".
 			 "<p>".$_POST['courriel']."</p>".
 			 "<p>".$_POST['sujet']."</p>".
 			 "<p>".$_POST['question']."</p>"
-		?>
+		*/
+        $nom = $_POST['nom'];
+        $courriel = $_POST['courriel'];
+        $sujet = $_POST['sujet'];
+        $question = $_POST['question'];
+        $Email->sentMail($nom,$courriel,$sujet,$question);
+        ?>
 	</body>
 </html>
---
+++ gestionnaire_commentaire.php
@@ -0,0 +1,14 @@
+<?php
+class mail{
+    function sentMail($nom,$courriel,$sujet,$question){
+        $header="MIME-Version: 1.0\r\n";
+		$header.='From:"'.$nom.'""'.$courriel.'"'."\n";
+		$header.='Content-Type:text/html; charset="uft-8"'."\n";
+		$header.='Content-Transfer-Encoding: 8bit';
+		mail("marc-etienne-pepin@hotmail.com",$sujet,$question,$header);
+		
+    }
+
+
+}
+?>
