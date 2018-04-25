accf79287c83a500cb95e36ad59e524ffa95f909
bmw546 <marc-etienne-pepin@hotmail.com>
2018-04-25 14:16:04 -0400

    ADDED notification courriel

---
+++ CtrlCourrielNotification.php
@@ -0,0 +1,26 @@
+<?php
+/*****************************************************************
+Fichier : CtrlCourrielNotification.php
+Auteur : Marc-Étienne Pépin
+Fonctionnalité : Sert Recupérer les information du forumlaire HTML
+et de les formatter et envoyer a la fonction pour notification de courriel et SMS
+Date : 2018-04-25
+
+Vérification :
+Date               Nom                   Approuvé
+===========================================================
+
+
+
+Historique de modifications :
+Date               Nom                   Description
+===========================================================
+
+ *****************************************************************/
+include("GestionnaireCourrielNotification.php");
+$Email = new CourrielNotification();
+$courriel = $_POST['email'];
+$telephone = $_POST['noTelephone'];
+$sms = $_POST['sms'];
+$notification = $_POST['notification'];
+?>
