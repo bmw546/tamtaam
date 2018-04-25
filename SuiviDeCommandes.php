c4615e8373435b7200179dd940f98d23bb7f31c4
DESKTOP-OHJLOHD\TheTh <thethousandbeanster@hotmail.com>
2018-04-25 09:49:22 -0400

    Suivi update

--- SuiviDeCommandes.php
+++ SuiviDeCommandes.php
@@ -15,11 +15,26 @@ Date                    Nom             Description
 ***********************************************-->
 <!DOCTYPE html>
 <html>
+
+	<?php
+		$datePrevue="Mai 1";
+		$etatCommande="Limbo";
+		$adresse="123 somewhere";
+		$montant=30;
+	?>
 	<head>
 		<meta charset='utf-8'/>
 		<title>cible suivi des commandes</title>
 	</head>
 	<body>
-		Number is <?php echo $_GET["orderNumber"];	?>
+		Numéro de commande :<?php echo $_POST["orderNumber"];	?>
+		<br>
+		Date de livraison prévue : <?php echo $datePrevue;?>
+		<br>
+		État de la commande : <?php echo $etatCommande;?>
+		<br>
+		Adresse de livraison : <?php echo $adresse;?>
+		<br>
+		Montant : <?php echo $montant;?>$
 	</body>
 </html>
