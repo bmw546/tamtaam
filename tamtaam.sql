a34b4936f6072acca94f55a7777783a1e1628f79
Jlapointe01 <34103691+Jlapointe01@users.noreply.github.com>
2018-04-29 17:32:27 -0400

    nouveau script sql

--- tamtaam.sql
+++ tamtaam.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 26, 2018 at 03:31 PM
+-- Generation Time: Apr 29, 2018 at 11:26 PM
 -- Server version: 10.1.31-MariaDB
 -- PHP Version: 7.2.4
 
@@ -37,6 +37,12 @@ CREATE TABLE `client` (
   `telephone` bigint(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Dumping data for table `client`
+--
+
+INSERT INTO `client` (`id_client`, `nom_utilisateur`, `mot_de_passe`, `adresse_email`, `adresse`, `telephone`) VALUES
+
 -- --------------------------------------------------------
 
 --
@@ -49,9 +55,16 @@ CREATE TABLE `commande` (
   `id_etat` int(11) NOT NULL,
   `id_type_commande` int(11) NOT NULL,
   `date` date NOT NULL,
-  `montant` date NOT NULL,
+  `montant` float NOT NULL,
   `nom` varchar(32) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `commande`
+--
+
+INSERT INTO `commande` (`id_commande`, `id_client`, `id_etat`, `id_type_commande`, `date`, `montant`, `nom`) VALUES
+(10, 1, 1, 1, '2018-04-26', 0, 'Test'),
 
 -- --------------------------------------------------------
 
@@ -64,6 +77,14 @@ CREATE TABLE `etat_commande` (
   `nom_etat` varchar(16) NOT NULL,
   `description_etat` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `etat_commande`
+--
+
+INSERT INTO `etat_commande` (`id_etat`, `nom_etat`, `description_etat`) VALUES
+(1, 'attente_paiement', 'Le client doit payer sa commande.'),
+(2, 'paiement_fait', 'Le paiement a été effectué.');
 
 -- --------------------------------------------------------
 
@@ -94,6 +115,13 @@ CREATE TABLE `livraison` (
   `date_livraison_prevue` date DEFAULT NULL,
   `date_livraison_reel` date DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `livraison`
+--
+
+INSERT INTO `livraison` (`id_livraison`, `id_commande`, `adresse`, `adresse_latitude`, `adresse_longitude`, `date_livraison_prevue`, `date_livraison_reel`) VALUES
+
 
 -- --------------------------------------------------------
 
@@ -159,6 +187,13 @@ CREATE TABLE `ta_produit_commande` (
   `nb_produit` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Dumping data for table `ta_produit_commande`
+--
+
+INSERT INTO `ta_produit_commande` (`id_produit`, `id_commande`, `nb_produit`) VALUES
+
+
 -- --------------------------------------------------------
 
 --
@@ -191,6 +226,13 @@ CREATE TABLE `type_commande` (
   `id_type_commande` int(11) NOT NULL,
   `description_type_commande` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `type_commande`
+--
+
+INSERT INTO `type_commande` (`id_type_commande`, `description_type_commande`) VALUES
+(1, 'Livraison');
 
 -- --------------------------------------------------------
 
@@ -302,19 +344,19 @@ ALTER TABLE `type_rabais`
 -- AUTO_INCREMENT for table `client`
 --
 ALTER TABLE `client`
-  MODIFY `id_client` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_client` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `commande`
 --
 ALTER TABLE `commande`
-  MODIFY `id_commande` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id_commande` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=75;
 
 --
 -- AUTO_INCREMENT for table `etat_commande`
 --
 ALTER TABLE `etat_commande`
-  MODIFY `id_etat` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_etat` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `evenement`
@@ -326,7 +368,7 @@ ALTER TABLE `evenement`
 -- AUTO_INCREMENT for table `livraison`
 --
 ALTER TABLE `livraison`
-  MODIFY `id_livraison` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id_livraison` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=73;
 
 --
 -- AUTO_INCREMENT for table `produit`
