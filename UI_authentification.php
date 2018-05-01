54ecbf1c4c6d4e33e324bfaed0288f63853d253e
Rtbchrono <xblromtsarafo@hotmail.com>
2018-05-01 16:43:05 -0400

    update css ui_authentification

--- UI_authentification.php
+++ UI_authentification.php
@@ -15,48 +15,70 @@ Historique de modifications :
 Date               Nom                   Description
 ===========================================================
 2018-04-25		    Roméo			Ajout de commentaire, ajout des "required"
+2018-05-01          Roméo          Implémenté CSS
 ******************************************************************************************************-->
 <html>
-    <head>
-        <meta charset="utf-8" />
-        <title>Connexion</title>
-    </head>
-    <body style="margin:auto; width:950px;">
-    <header>
-        <h1 style="text-align:center;"><i>Connexion</i></h1>
-    </header>
-        <fieldset>
-            <form action="CtrlAuthentification.php" method="post">
-                <p>
-                    <label for="nom_utilisateur">Nom d'utilisateur: </label>
+<head>
+    <meta charset="utf-8" />
+    <link rel="stylesheet" href="HTML/css/style.css" />
+    <link href="https://fonts.googleapis.com/css?family=Playfair+Display" rel="stylesheet">
+    <title>Connexion</title>
+</head>
+
+<body class="inscriptionBody"">
+
+
+<nav class="entete col-12">
+    <img src="HTML/image/logo.png" alt="logo"/>
+    <ul>
+        <li class="menu col-2 col-t-2"><a href="#">Accueil</a></li>
+        <li class="pointMenu menu col-2 col-t-2"><a href="#">Nos produits</a></li>
+        <li class="pointMenu menu col-2 col-t-2"><a href="#">En savoir plus</a></li>
+        <li class="pointMenu menu col-2 col-t-2"><a href="#">Points de ventes</a></li>
+        <li class="pointMenu menu col-2 col-t-2"><a href="#">Nous joindre</a></li>
+    </ul>
+</nav>
+
+
+<section class="sectionInscription col-12">
+    <div class="inscriptionheader"><i>Connexion des utilisateurs</i></div>
+    <fieldset>
+        <form class="formInscription" action="CtrlAuthentification.php" method="post">
+
+            <div class="centerForm">
+                <p >
+                    <label class="label" for="nom_utilisateur">Nom d'utilisateur: </label>
                     <input type="text" name="nom_utilisateur" id="nom_utilisateur" required/>
                 </p>
                 <p>
-                  <label for="mot_de_passe">Mot de passe: </label>
-                  <input style="margin-left:25px;" type="password" name="mot_de_passe" id="mot_de_passe" required/>
+                    <label class="label" for="mot_de_passe">Mot de passe: </label>
+                    <input type="password" name="mot_de_passe" id="mot_de_passe" required/>
                 </p>
-                <p>
-                    <input  style="margin-left:80px; margin-right:30%; background-color:black; color:white; border-color:black;" name="connexion" type="submit" value="Se connecter"/>
-                    <button formnovalidate style=" margin-left:25%; margin-right:100px; background-color:black; color:white; border-color:black;" name="cancel" type="reset" value="Annuler">Annuler</button>
+
+                <p class="col-12">
+                    <input  class="btnInscrire btnStyle col-2" name="connexion" type="submit" value="Se connecter"/>
+                    <button class="btnInscrire btnStyle col-2" name="cancel" type="reset" value="Annuler">Annuler</button>
                 </p>
-            </form>
-            <center>
+            </div>
+        </form>
+        <footer class="inscriptionFooter">
             <?php
             $fullUrl = "http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
             if (strpos($fullUrl, "nomUtilisateurInvalide") == true) {
-              echo "<p style='color:red;'>". "Nom d'utilisateur invalide" ."</p>";
+                echo "<p style='color:red;'>". "Nom d'utilisateur invalide" ."</p>";
             }
             elseif (strpos($fullUrl, "mdpInvalide") == true) {
-              echo "<p style='color:red;'>". "Mot de passe invalide" ."</p>";
+                echo "<p style='color:red;'>". "Mot de passe invalide" ."</p>";
             }
-             ?>
-           </center>
-             <br>
-             <a style=" margin-left:90px; width:30%; display:block;float:left;" href="UI_Inscription.php">Créer un compte</a>
-             <a style="width:30%; display:block;float:left;" href="UI_motDePasseOublie.php">Mot de passe oublié</a>
-             <a style="width:30%; display:block;float:left;" href="UI_nomUtilisateurOublie.php">Nom d'utilisateur oublié</a>
-        </fieldset>
+            ?>
+            <br>
+            <a class="col-4 col-m-4" href="UI_Inscription.php">Créer un compte</a>
+            <a class="col-4 col-m-4" href="UI_motDePasseOublie.php">Mot de passe oublié</a>
+            <a class="col-4 col-m-4" href="UI_nomUtilisateurOublie.php">Nom d'utilisateur oublié</a>
+        </footer>
+    </fieldset>
+</section>
 
-    </body>
+</body>
 
 </html>
