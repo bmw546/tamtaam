71d1f51cae55536852a837b47a7419450a43e59c
idremi7 <32065191+idremi7@users.noreply.github.com>
2018-05-03 10:00:15 -0400

    UI_inscription ajout javascript temporaire
    ajout des Regex pour les champs

--- UI_Inscription.php
+++ UI_Inscription.php
@@ -1,4 +1,4 @@
-<!--******************************************************
+<!--***************************************************************************************************
 Fichier : UI_Inscription.php
 Auteur : Rémi Létourneau
 Fonctionnalité : Gestion des comptes utilisateurs
@@ -6,17 +6,18 @@ Date : 2018-04-23
 
 Vérification :
 Date                Nom 		          Approuvé
-=========================================================
+=====================================================================================================
 2018-04-29          Joel Lapointe         Oui
 
 Historique de modifications :
 Date                    Nom             Description
-=========================================================
+=====================================================================================================
 2018-04-25				Roméo            Modifié textfield pour password pour le champ mot de passe
 2018-04-29				Roméo 		     Rajouté les messages d'erreurs/succès
 2018-04-30             Rémi             Rajout lien avec css, ajout de style css
 2018-05-01             Rémi             Modification style css
-***********************************************************-->
+2018-05-03             Rémi             Ajout javascript temporaire pour valider les regex.
+******************************************************************************************************-->
 <html>
 	<head>
 		<meta charset="utf-8" />
@@ -46,16 +47,16 @@ Date                    Nom             Description
                 <input class="" type="text" name="user" id="user" size="30" required/>
 
                 <br><br>
-                <label class="label" for="adresse" ><b>Adresse :</b></label>
-                <input class="" type="text" name="adresse" id="adresse" onblur="checkadress('adresse')" size="30" required/>
+                <label class="label" for="adresse" id="lAdresse"><b>Adresse :</b></label>
+                <input class="" type="text" name="adresse" id="adresse" size="30" required onblur="check('adresse')"/>
 
                 <br><br>
-                <label class="label" for="email" ><b>E-mail :</b></label>
-                <input class="" type="text" name="email" id="email" size="30" required/>
+                <label class="label" for="email" id="lemail"><b>E-mail :</b></label>
+                <input class="" type="text" name="email" id="email" size="30" required  onblur="check('email')"/>
 
                 <br><br>
-                <label class="label" for="noTelephone" ><b>Numéro de téléphone :</b></label>
-                <input class="" type="text" name="noTelephone" id="noTelephone" placeholder="000 000 0000" size="30" required/>
+                <label class="label" for="noTelephone" id="lNoTelephone"><b>Numéro de téléphone :</b></label>
+                <input class="" type="text" name="noTelephone" id="noTelephone" placeholder="000 000 0000" size="30" required onblur="check('noTelephone')"/>
 
                 <br><br>
                 <label class="label" for="passwd" ><b>Mot de passe :</b></label>
@@ -64,13 +65,14 @@ Date                    Nom             Description
                 <br><br>
                 <label class="label" for="confirmer" ><b>Confirmer votre mot de passe :</b></label>
                 <input class="" type="password" name="confirmer" id="confirmer" size="30" required/>
-
                 <br><br>
-
+                <!-------------------- Validation anti-robot captcha------------------------------>
                 <p><img src="verif_code_gen.php" alt="Code de vérification" /></p>
                 <br>
-                <p><label>Merci de retaper le code de l'image ci-dessus</label> : <input type="text" name="verif_code" /></p>
-
+                <label for="verif_code">Merci de retaper le code de l'image ci-dessus :</label>
+                <input type="text" name="verif_code" id="verif_code"/>
+                <br><br>
+                <!-------------------------------------------------------------------------------->
                 <input class="btnInscrire btnStyle " name="inscrire" type="submit" value="S'inscrire"/>
                 <button class="btnInscrire btnStyle " type="reset" name="cancel" value="Annuler">Effacer</button>
 
@@ -104,15 +106,51 @@ Date                    Nom             Description
 </html>
 
 <script>
-    function checkadress(who){
+    //peut être simplifier en mettant en parametre : (who, label et regex)
+    /**
+     * valide les informations des champs d'un formulaire
+     * @param who string  le nom de l'id du champ
+     */
+    function check(who){
         var myRegex;
-        myRegex = /((([0-9]+))(\w+(\s\w+){2,})(,)?(\s{0,})([a-z]{0,})(\s{0,})(,)?(\s{0,})([a-z]{0,})(\s{0,})([a-z][0-9][a-z] ?[0-9][a-z][0-9])|(([a-z][0-9][a-z])-([0-9][a-z][0-9]))|([a-z][0-9][a-z][0-9][a-z][0-9]))/i;
+        var value;
 
-        var value = document.getElementById((who)).value;
-        if(myRegex.test(value)){
+        if(who === 'adresse'){
+
+            myRegex = /((([0-9]+))(\w+(\s\w+){2,})(,)?(\s{0,})([a-z]{0,})(\s{0,})(,)?(\s{0,})([a-z]{0,})(\s{0,})([a-z][0-9][a-z] ?[0-9][a-z][0-9])|(([a-z][0-9][a-z])-([0-9][a-z][0-9]))|([a-z][0-9][a-z][0-9][a-z][0-9]))/i;
+            value = document.getElementById((who)).value;
+            if(!myRegex.test(value)){
+                document.getElementById('lAdresse').style.color = 'red';
+                document.getElementById('adresse').select();
+                //alert("ERREUR l'adresse contient des information invalide :"+ value +" veuillez faire comme ceci ex: 475 Rue du Cegep, Sherbrooke, QC J1A 4K1 ");
+            }
+            else{
+                document.getElementById('lAdresse').style.color = 'black';
+            }
         }
-        else{
-            alert("ERREUR l'adresse contient des information invalide :"+ value +" veuillez faire comme ceci ex: 475 Rue du Cegep, Sherbrooke, QC J1A 4K1 ");
+        else if (who === 'email'){
+
+            myRegex = /([a-z0-9\.-_]+)@([a-z0-9]+)\.([a-z]{2,})|(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/i;
+            value = document.getElementById((who)).value;
+            if(!myRegex.test(value)){
+                document.getElementById('lemail').style.color = 'red';
+                document.getElementById('email').select();
+                //alert("ERREUR l'email contient des informations invalides :"+ value +" nom@hotmail.com");
+            }else{
+                document.getElementById('lemail').style.color = 'black';
+            }
+        }
+        else if (who === 'noTelephone'){
+
+            myRegex = /(\(?[0-9]{3}\)?)? ?\.?-?[0-9]{3}\.?-?[0-9]{4}/;
+            value = document.getElementById((who)).value;
+            if(!myRegex.test(value)){
+                document.getElementById('lNoTelephone').style.color = 'red';
+                document.getElementById('noTelephone').select();
+                //alert("ERREUR l'email contient des informations invalides :"+ value +" nom@hotmail.com");
+            }else{
+                document.getElementById('lNoTelephone').style.color = 'black';
+            }
         }
     }
 </script>
