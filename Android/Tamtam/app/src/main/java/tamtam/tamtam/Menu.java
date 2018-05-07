ef87d867392e7747a98816b25b5179771d6589cd
idremi7 <32065191+idremi7@users.noreply.github.com>
2018-05-07 16:00:54 -0400

    update Menu 0.0.7: open new activity

--- Android/Tamtam/app/src/main/java/tamtam/tamtam/Menu.java
+++ Android/Tamtam/app/src/main/java/tamtam/tamtam/Menu.java
@@ -1,13 +1,24 @@
 package tamtam.tamtam;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class menu extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.ui_menu);
+
+    }
+
+    public void showActivity(View view){
+        Intent intent = new Intent(this, ui_client.class);
+
+        startActivity(intent);
     }
 }
---
+++ Android/Tamtam/app/src/main/java/tamtam/tamtam/moteur_requete_bd.java
@@ -0,0 +1,13 @@
+package tamtam.tamtam;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+
+public class moteur_requete_bd {
+
+    private SQLiteDatabase tamtamDataBase;
+
+    public moteur_requete_bd() {
+
+    }
+}
