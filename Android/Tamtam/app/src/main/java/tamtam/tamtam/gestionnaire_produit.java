373a67f25265b9d271b65e128441361e71527a4f
Jlapointe01 <34103691+Jlapointe01@users.noreply.github.com>
2018-05-07 15:21:49 -0400

    ui_produit

--- Android/Tamtam/app/src/main/java/tamtam/tamtam/gestionnaire_produit.java
+++ Android/Tamtam/app/src/main/java/tamtam/tamtam/gestionnaire_produit.java
@@ -1,4 +1,13 @@
 package tamtam.tamtam;
 
-public class gestionnaire_produit {
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class gestionnaire_produit extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gestionnaire_produit);
+    }
 }
