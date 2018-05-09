7d13ec9aff78df35742b7434ac47c1195a77e2e6
idremi7 <32065191+idremi7@users.noreply.github.com>
2018-05-08 21:10:58 -0400

    MoteurRequeteBD Update 0.1

--- Android/Tamtam/app/src/main/java/tamtam/tamtam/moteur_requete_bd.java
+++ Android/Tamtam/app/src/main/java/tamtam/tamtam/moteur_requete_bd.java
@@ -5,15 +5,75 @@ import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 public class Moteur_requete_bd extends SQLiteOpenHelper{
-    public static final String DATABASE_NAME = "tamtaam.db";
 
-    public static final String TABLE_NAME = "client_table";
-    public static final String COL_1 = "ID";
-    public static final String COL_2 = "NOM";
-    public static final String COL_3 = "MOT_DE_PASSE";
-    public static final String COL_4 = "COURRIEL";
-    public static final String COL_5 = "ADRESSE";
-    public static final String COL_6 = "TELEPHONE";
+    // Logcat tag
+    private static final String LOG = "Moteur_requete_bd";
+
+    // Database Version
+    private static final int DATABASE_VERSION = 1;
+
+    // Database Name
+    private static final String DATABASE_NAME = "tamtaam";
+
+    // Table Names
+    private static final String TABLE_CLIENT = "client";
+    private static final String TABLE_TYPE_RABAIS = "type_rabais";
+    private static final String TABLE_RABAIS = "rabais";
+    private static final String TABLE_PRODUIT = "produit";
+    private static final String TABLE_RABAIS_PRODUIT = "ta_rabais_produit";
+
+    // Common column names
+    private static final String KEY_ID = "id";
+    private static final String KEY_NOM = "nom";
+    private static final String KEY_DESCRIPTION = "description";
+
+    // CLIENT Table - column names
+    private static final String KEY_MOT_DE_PASSE = "mot_de_passe";
+    private static final String KEY_COURRIEL = "courriel";
+    private static final String KEY_ADRESSE = "adresse";
+    private static final String KEY_TELEPHONE = "telephone";
+
+    // RABAIS Table - column names
+    private static final String KEY_CODE = "code";
+    private static final String KEY_MONTANT = "montant";
+    private static final String KEY_ID_TYPE = "id_type";
+
+    // PRODUIT Table - column names
+    private static final String KEY_PRIX = "prix";
+
+    // RABAIS_PRODUIT Table - column names
+    private static final String KEY_ID_PRODUIT = "id_produit";
+    private static final String KEY_CODE_RABAIS = "code_rabais";
+
+    // Table Create Statements
+    // Client table create statement
+    private static final String CREATE_TABLE_CLIENT = "CREATE TABLE "
+            + TABLE_CLIENT + "(" + KEY_ID + " INTEGER PRIMARY KEY AUTOINCREMENT," + KEY_NOM
+            + " VARCHAR(32)," + KEY_MOT_DE_PASSE + " VARCHAR(32)," + KEY_COURRIEL
+            + " VARCHAR(64)," + KEY_ADRESSE + " VARCHAR(64)," + KEY_TELEPHONE + " BIGINT" + ")";
+
+    // type_rabais table create statement
+    private static final String CREATE_TABLE_TYPE_RABAIS = "CREATE TABLE " + TABLE_TYPE_RABAIS
+            + "(" + KEY_ID + " INTEGER PRIMARY KEY AUTOINCREMENT," + KEY_NOM + " VARCHAR(32),"
+            + KEY_DESCRIPTION + " TEXT" + ")";
+
+    // rabais table create statement
+    private static final String CREATE_TABLE_RABAIS = "CREATE TABLE " + TABLE_RABAIS
+            + "(" + KEY_CODE_RABAIS + " VARCHAR(8) PRIMARY KEY," + KEY_MONTANT + " FLOAT,"
+            + KEY_ID_TYPE + " INTEGER," + KEY_DESCRIPTION + " TEXT"
+            + "FOREIGN KEY (" + KEY_ID_TYPE + ") REFERENCES " + TABLE_TYPE_RABAIS + "(" + KEY_ID +")" + ")";
+
+    // produit table create statement
+    private static final String CREATE_TABLE_PRODUIT = "CREATE TABLE " + TABLE_PRODUIT
+            + "(" + KEY_ID + " INTEGER PRIMARY KEY AUTOINCREMENT," + KEY_NOM + " VARCHAR(32),"
+            + KEY_DESCRIPTION + " TEXT" + KEY_PRIX + " FLOAT" +")";
+
+    // rabais_produit table create statement
+    private static final String CREATE_TABLE_RABAIS_PRODUIT = "CREATE TABLE "
+            + TABLE_RABAIS_PRODUIT + "(" + KEY_ID_PRODUIT + " INTEGER PRIMARY KEY,"
+            + KEY_CODE_RABAIS + " VARCHAR(8)"
+            + " FOREIGN KEY (" + KEY_ID_PRODUIT + ") REFERENCES "+ TABLE_PRODUIT + "("+ KEY_ID + ")"
+            + " FOREIGN KEY (" + KEY_CODE_RABAIS + ") REFERENCES "+ TABLE_RABAIS + "("+ KEY_CODE_RABAIS + ")"+")";
 
     public Moteur_requete_bd(Context context) {
         super(context, DATABASE_NAME, null, 1);
@@ -22,18 +82,22 @@ public class Moteur_requete_bd extends SQLiteOpenHelper{
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        db.execSQL("CREATE TABLE " + TABLE_NAME + "(ID INTEGER PRIMARY KEY AUTOINCREMENT, " +
-                " NOM VARCHAR(32)," +
-                " MOT_DE_PASSE VARCHAR(32)," +
-                " COURRIEL VARCHAR(64)," +
-                " ADRESSE VARCHAR(64)," +
-                " TELEPHONE BIGINT)");
+
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
+        db.execSQL("DROP TABLE IF EXISTS client_table");
+        db.execSQL("DROP TABLE IF EXISTS rabais_table");
+        db.execSQL("DROP TABLE IF EXISTS type_rabais_table");
+
+        onCreate(db);
     }
 
-
+    // closing database
+    public void closeDB() {
+        SQLiteDatabase db = this.getReadableDatabase();
+        if (db != null && db.isOpen())
+            db.close();
+    }
 }
