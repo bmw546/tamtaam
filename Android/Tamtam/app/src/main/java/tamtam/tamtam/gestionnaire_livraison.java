613a0069f882fe25400fa3b397d1cf6fc7d39d28
bmw546 <marc-etienne-pepin@hotmail.com>
2018-05-09 16:22:12 -0400

    livraison avancer , client presque terminé

--- Android/Tamtam/app/src/main/java/tamtam/tamtam/gestionnaire_livraison.java
+++ Android/Tamtam/app/src/main/java/tamtam/tamtam/gestionnaire_livraison.java
@@ -1,7 +1,8 @@
 package tamtam.tamtam;
 
-import android.support.v7.app.AppCompatActivity;
 import android.app.ListActivity;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
@@ -9,16 +10,17 @@ import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
-import android.widget.AdapterView.OnItemClickListener;
-
-import java.util.ArrayList;
 
 public class gestionnaire_livraison extends ListActivity {
+
+    private moteur_requete_bd myBd;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-       //setContentView(R.layout.activity_gestionnaire_livraison);
-        String[] FRUITS = new String[] { "Apple", "Avocado", "Banana",
+        //setContentView(R.layout.ui_client);
+        myBd = new moteur_requete_bd(this); //create the local database
+        final String[] FRUITS = new String[] { "Apple", "Avocado", "Banana",
                 "Blueberry", "Coconut", "Durian", "Guava", "Kiwifruit",
                 "Jackfruit", "Mango", "Olive", "Pear", "Sugar-apple" };
         setListAdapter(new ArrayAdapter<String>(this, R.layout.ui_livraison,FRUITS));
@@ -30,10 +32,20 @@ public class gestionnaire_livraison extends ListActivity {
             public void onItemClick(AdapterView<?> parent, View view,
                                     int position, long id) {
                 // When clicked, show a toast with the TextView text
-                Toast.makeText(getApplicationContext(),
+               /* Toast.makeText(getApplicationContext(),
                         ((TextView) view).getText(), Toast.LENGTH_SHORT).show();
+                */
+                Intent intent = new Intent(view.getContext(), livraison_detail.class);
+                Bundle b = new Bundle();
+                b.putInt("key", position); //Your id
+                b.putString("nom",FRUITS[position]);
+                b.putString("adresse",FRUITS[position]+" at fruit.com");
+                b.putDouble("longitude",45.411701);
+                b.putDouble("lat",-71.886361);
+                intent.putExtras(b); //Put your id to your next Intent
+                startActivity(intent);
+                finish();
             }
         });
-
     }
 }
