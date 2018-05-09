92c53e8b08e54f3cf655e369a91cfd4214c1f556
benoit-pc\Benoit <thethousandbeanster@hotmail.com>
2018-05-09 16:23:00 -0400

    UI rapport

--- Android/Tamtam/app/src/main/java/tamtam/tamtam/GestionnaireRapport.java
+++ Android/Tamtam/app/src/main/java/tamtam/tamtam/GestionnaireRapport.java
@@ -2,12 +2,25 @@ package tamtam.tamtam;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.view.View;
+import android.widget.EditText;
 
 public class GestionnaireRapport extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.ui_rapport);
+
+        final Button button = findViewById(R.id.buttonVisionner);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Code here executes on main thread after user presses button
+                EditText txtDateDebut = findViewById(R.id.txt_dateDebut);
+            }
+        });
+
     }
 }
