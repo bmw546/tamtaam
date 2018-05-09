af67c92771db632a41ac8e9ad04485d373d0c968
bmw546 <marc-etienne-pepin@hotmail.com>
2018-05-09 15:03:31 -0400

    MAP UPTADED

--- Android/Tamtam/app/src/main/java/tamtam/tamtam/detail_client.java
+++ Android/Tamtam/app/src/main/java/tamtam/tamtam/detail_client.java
@@ -1,21 +1,37 @@
 package tamtam.tamtam;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.LocationManager;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import java.util.List;
+
 public class detail_client extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Bundle b = getIntent().getExtras();
+        int value = -1; // or other values
+        if(b != null){
+            value = b.getInt("key");
+        }
+
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail_client);
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
@@ -23,24 +39,15 @@ public class detail_client extends FragmentActivity implements OnMapReadyCallbac
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
     }
-
-
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        // Add a marker in client and move the camera
+        LatLng client = new LatLng(45.411701, -71.886361);
+        mMap.addMarker(new MarkerOptions().position(client).title("Adresse client"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(client,17.0f));
+        mMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
+        //mMap.addMarker(mMarker);
     }
 }
