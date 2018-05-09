42ede1ac07f56f795c2a79f3fd860a0cd13d8621
bmw546 <marc-etienne-pepin@hotmail.com>
2018-05-09 15:53:53 -0400

    no message

--- Android/Tamtam/app/src/main/java/tamtam/tamtam/detail_client.java
+++ Android/Tamtam/app/src/main/java/tamtam/tamtam/detail_client.java
@@ -7,8 +7,10 @@ import android.location.LocationManager;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.common.images.ImageRequest;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
@@ -23,14 +25,31 @@ import java.util.List;
 public class detail_client extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    private double longitude;
+    private double latitude;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         Bundle b = getIntent().getExtras();
-        int value = -1; // or other values
-        if(b != null){
-            value = b.getInt("key");
-        }
 
+        TextView name = (TextView) findViewById(R.id.nom);
+        TextView numero = (TextView) findViewById(R.id.Numero);
+        TextView adress = (TextView) findViewById(R.id.adresse);
+        int id = -1; // or other values
+        String nom = "blank";
+        String adresse="blank";
+        longitude = 0;
+        latitude = 0;
+        if(b != null){
+            id = b.getInt("key");
+           nom = b.getString("nom");
+            adresse= b.getString("adresse");
+            longitude = b.getDouble("longitude");
+            latitude = b.getDouble("lat");
+        }
+       // name.setText(nom);
+        //numero.setText(""+id);
+        adress.setText("blbalbalbal");
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail_client);
@@ -44,7 +63,7 @@ public class detail_client extends FragmentActivity implements OnMapReadyCallbac
         mMap = googleMap;
 
         // Add a marker in client and move the camera
-        LatLng client = new LatLng(45.411701, -71.886361);
+        LatLng client = new LatLng(longitude,latitude);
         mMap.addMarker(new MarkerOptions().position(client).title("Adresse client"));
         mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(client,17.0f));
         mMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
