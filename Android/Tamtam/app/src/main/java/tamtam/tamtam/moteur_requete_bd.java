7f35fb4dac480ebcd1c9ac8ab1cb3094a3f96d6a
idremi7 <32065191+idremi7@users.noreply.github.com>
2018-05-09 08:35:41 -0400

    MoteurRequeteBD 0.2
    avec  update ui_client 0.1

--- Android/Tamtam/app/src/main/java/tamtam/tamtam/moteur_requete_bd.java
+++ Android/Tamtam/app/src/main/java/tamtam/tamtam/moteur_requete_bd.java
@@ -21,8 +21,17 @@ public class Moteur_requete_bd extends SQLiteOpenHelper{
     private static final String TABLE_RABAIS = "rabais";
     private static final String TABLE_PRODUIT = "produit";
     private static final String TABLE_RABAIS_PRODUIT = "ta_rabais_produit";
+    private static final String TABLE_EVENEMENT = "evenement";
+    private static final String TABLE_RECETTE = "recette";
+    private static final String TABLE_PRODUIT_RECETTE = "ta_produit_recette";
+    private static final String TABLE_TYPE_COMMANDE = "type_commande";
+    private static final String TABLE_ETAT_COMMANDE = "etat_commande";
+    private static final String TABLE_COMMANDE = "commande";
+    private static final String TABLE_PRODUIT_COMMANDE = "ta_produit_commande";
+    private static final String TABLE_LIVRAISON = "livraison";
+    private static final String TABLE_NOTIFICATION = "notification";
 
-    // Common column names
+    // Common column names - Recette, type-rabais, etat-commande, type-commande
     private static final String KEY_ID = "id";
     private static final String KEY_NOM = "nom";
     private static final String KEY_DESCRIPTION = "description";
@@ -35,15 +44,44 @@ public class Moteur_requete_bd extends SQLiteOpenHelper{
 
     // RABAIS Table - column names
     private static final String KEY_CODE = "code";
-    private static final String KEY_MONTANT = "montant";
+    private static final String KEY_MONTANT_RABAIS = "montant_rabais";
     private static final String KEY_ID_TYPE = "id_type";
 
     // PRODUIT Table - column names
     private static final String KEY_PRIX = "prix";
 
-    // RABAIS_PRODUIT Table - column names
+    // Table association - foreign key - column names - PRODUIT_RECETTE - RABAIS_PRODUIT - PRODUIT_COMMANDE
     private static final String KEY_ID_PRODUIT = "id_produit";
     private static final String KEY_CODE_RABAIS = "code_rabais";
+    private static final String KEY_ID_RECETTE = "id_recette";
+    private static final String KEY_ID_COMMANDE = "id_commande";
+    private static final String KEY_NB_PRODUIT = "nb_produit";
+    private static final String KEY_ID_CLIENT = "id_client";
+
+    // EVENEMENT Table - column names
+    private static final String KEY_DATE_DEBUT = "date_debut";
+    private static final String KEY_DATE_FIN = "date_fin";
+
+    // COMMANDE Table - column names
+    private static final String KEY_ID_ETAT = "id_etat";
+    private static final String KEY_ID_TYPE_COMMANDE = "id_type_commande";
+    private static final String KEY_DATE = "id_type_commande";
+    private static final String KEY_MONTANT_COMMANDE = "montant_commande";
+    private static final String KEY_NOM_PERSONNE = "nom_personne";
+
+    // Livraison Table - column names
+    private static final String KEY_ADRESSE_LIVRAISON = "adresse_livraison";
+    private static final String KEY_ADRESSE_LATITUDE = "adresse_latitude";
+    private static final String KEY_ADRESSE_LONGITUDE = "adresse_logitude";
+    private static final String KEY_DATE_PREVUE = "date_prevue";
+    private static final String KEY_DATE_REEL = "date_reel";
+
+    // Notification Table - column names
+    private static final String KEY_SMS = "sms";
+    private static final String KEY_NOTIFICATION = "notification";
+    private static final String KEY_NOUVELLE = "nouvelle";
+    private static final String KEY_RECEPTION = "reception";
+    private static final String KEY_ETAT = "etat";
 
     // Table Create Statements
     // Client table create statement
@@ -59,7 +97,7 @@ public class Moteur_requete_bd extends SQLiteOpenHelper{
 
     // rabais table create statement
     private static final String CREATE_TABLE_RABAIS = "CREATE TABLE " + TABLE_RABAIS
-            + "(" + KEY_CODE_RABAIS + " VARCHAR(8) PRIMARY KEY," + KEY_MONTANT + " FLOAT,"
+            + "(" + KEY_CODE_RABAIS + " VARCHAR(8) PRIMARY KEY," + KEY_MONTANT_RABAIS + " FLOAT,"
             + KEY_ID_TYPE + " INTEGER," + KEY_DESCRIPTION + " TEXT"
             + "FOREIGN KEY (" + KEY_ID_TYPE + ") REFERENCES " + TABLE_TYPE_RABAIS + "(" + KEY_ID +")" + ")";
 
