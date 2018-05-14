1a271316031e3bffed1013a7c5961f26d07e6c7f
Rtbchrono <xblromtsarafo@hotmail.com>
2018-05-14 08:52:25 -0400

    done sql, but moteurBD not working
    Je crois qu'il faut juste que je copie
    mon code dans le master et
    ça va regler le problème

--- Android/Tamtam/app/src/main/java/tamtam/tamtam/GestionnaireRabais.java
+++ Android/Tamtam/app/src/main/java/tamtam/tamtam/GestionnaireRabais.java
@@ -26,44 +26,45 @@ public class GestionnaireRabais {
 
     public void selectRabais(){
         //va select les rabais dans la bd et les mettre dans l'arraylist
-        Cursor c = bd.execution_with_return("SELECT * FROM " + bd.getTableRabais());
-        if (c.getCount() > 0){
-            for (c.moveToFirst(); !c.isAfterLast(); c.moveToNext()) {
+        Cursor result = bd.execution_with_return("SELECT * FROM " + bd.getTableRabais());
+        if (result.getCount() > 0){
+            for (result.moveToFirst(); !result.isAfterLast(); result.moveToNext()) {
                 rabais r = new rabais();
                 //public rabais(String code_rabais, float montant, String description, String dateDebut, String dateFin, char type) {
-                r.setCode(c.getString(c.getColumnIndex("code")));
-                r.setMontant(c.getFloat(c.getColumnIndex("montant_rabais")));
-                r.setDescription(c.getString(c.getColumnIndex("description")));
-                int id_type = c.getInt(c.getColumnIndex("id_type"));
+                r.setCode(result.getString(result.getColumnIndex("code")));
+                r.setMontant(result.getFloat(result.getColumnIndex("montant_rabais")));
+                r.setDescription(result.getString(result.getColumnIndex("description")));
+                int id_type = result.getInt(result.getColumnIndex("id_type"));
                 if (id_type == 1){
                     r.setType('$');
                 }else{
                     r.setType('%');
                 }
-                r.setDateDebut(c.getString(c.getColumnIndex("date_debut")));
-                r.setDateFin(c.getString(c.getColumnIndex("date_fin")));
+                r.setDateDebut(result.getString(result.getColumnIndex("date_debut")));
+                r.setDateFin(result.getString(result.getColumnIndex("date_fin")));
 
                 listeRabais.add(r);
             }
 
         }
-        c.close();
+        result.close();
     }
+
     public Boolean ajouterRabais(rabais r){
 
-//        for(int i =0; i< listeRabais.size(); i++){
-//            if (listeRabais.get(i).getCode() == r.getCode())
-//            {
-//                return false;
-//            }
-//        }
-//
-//        listeRabais.add(r);
-//
-//      bd.execution("INSERT INTO " + bd.getTableRabais() + "(code, montant_rabais, id_type, description, date_debut, date_fin) " +
-//        "VALUES ('" + r.getCode() + "', " + r.getMontant() + "," + r.getNoType() + ", '"+ r.getDescription() +"', " +
-//                " '"+ r.getDateDebut() + "', '" + r.getDateFin() + "')");
-//
+        for(int i =0; i< listeRabais.size(); i++){
+            if (listeRabais.get(i).getCode() == r.getCode())
+            {
+                return false;
+            }
+        }
+
+        listeRabais.add(r);
+
+      bd.execution("INSERT INTO " + bd.getTableRabais() + "(code, montant_rabais, id_type, description, date_debut, date_fin) " +
+        "VALUES ('" + r.getCode() + "', " + r.getMontant() + "," + r.getNoType() + ", '"+ r.getDescription() +"', " +
+                " '"+ r.getDateDebut() + "', '" + r.getDateFin() + "')");
+
       return true;
     }
 
@@ -71,17 +72,27 @@ public class GestionnaireRabais {
 
         //delete where code = r.getCode();
         listeRabais.remove(r);
-
-
+        bd.execution("DELETE FROM" + bd.getTableRabais() + " WHERE code =" + "'" + r.getCode() + "'");
     }
 
     public Boolean modifierRabais(String oldCode,rabais r){
-        //cherche le rabais dans l'array list correspondant au vieux code et modifie le bon rabais
-
+        int idNewcode = 0;
+        for (int i = 0; i < listeRabais.size(); i++){
+            if (listeRabais.get(i).getCode() == r.getCode()){
+                return  false;
+            }
+            if (listeRabais.get(i).getCode() == oldCode){
+                idNewcode = i;
+            }
+        }
         //pour bd: set rabais where code = oldcode, etc.
 
         //si un autre rabais a le meme code
         //return false
+        listeRabais.set(idNewcode, r);
+        //code, montant_rabais, id_type, description, date_debut, date_fin) "
+        bd.execution("UPDATE" + bd.getTableRabais() + "SET 'code' = '" +r.getCode()+ "', 'id_type' = '" + r.getNoType() + "', 'description' = '" +
+             r.getDescription() + "', 'date_debut' = '" +r.getDateDebut() + "', 'date_fin' = '" + r.getDateFin() + "'");
 
         return true;
     }
